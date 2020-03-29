4924d6ea8c477deee28cafb4bfa536f5d8e1ccdd
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-03-29 21:17:58 +0530

    check server port

--- Server/server.js
+++ Server/server.js
@@ -8,3 +8,13 @@ const app = express();
 app.use(bodyParser.json()); //reading data with json format
 app.use(bodyParser.urlencoded({extended:false}));//when it false extended, it read all sort of file formats
 app.use(morgon("dev"));//this will log all request to the terminal
+
+app.get("/", (req, res, next)=>{
+    res.json({
+        "message":"Hello World!"
+    })
+});
+
+app.listen(3030, err=>{
+    console.log("Server is running.. jfdjksj");
+});

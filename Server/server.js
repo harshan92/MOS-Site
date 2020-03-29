4b6e0545ff9cd9c2c977557dd3d393b4f2755143
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-03-29 21:07:04 +0530

    implement the server

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+const express=require("express"); // framework for http routes
+const morgon=require("morgan");//http request logger, middleware , 
+const bodyParser=require("body-parser");// request data reader
+const mongoose=require("mongoose");//mongo db handler, db agent
+//instance of express app
+const app = express();
+
+app.use(bodyParser.json()); //reading data with json format
+app.use(bodyParser.urlencoded({extended:false}));//when it false extended, it read all sort of file formats
+app.use(morgon("dev"));//this will log all request to the terminal

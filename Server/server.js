1454ebb95dbd639a267614f554c45d9d5abe0d83
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-03-29 21:42:58 +0530

    configure mongo db server

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,19 @@ const express=require("express"); // framework for http routes
 const morgon=require("morgan");//http request logger, middleware , 
 const bodyParser=require("body-parser");// request data reader
 const mongoose=require("mongoose");//mongo db handler, db agent
+
+const config=require("./config");
+
 //instance of express app
 const app = express();
+
+mongoose.connect(config.database, err=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Connected to db server");
+    }
+});
 
 app.use(bodyParser.json()); //reading data with json format
 app.use(bodyParser.urlencoded({extended:false}));//when it false extended, it read all sort of file formats
@@ -12,9 +23,9 @@ app.use(morgon("dev"));//this will log all request to the terminal
 app.get("/", (req, res, next)=>{
     res.json({
         "message":"Hello World!"
-    })
+    });
 });
 
 app.listen(3030, err=>{
-    console.log("Server is running.. jfdjksj");
+    console.log("Server is running.. port: "+config.port);
 });

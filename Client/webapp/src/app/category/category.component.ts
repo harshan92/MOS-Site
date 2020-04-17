2fb88b25e553094eb136b6586ed67565827a20bd
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-18 01:58:47 +0530

    Implement category component 2

--- Client/webapp/src/app/category/category.component.ts
+++ Client/webapp/src/app/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {DataService} from "../data.service";
+import {RestApiService} from "../rest-api.service";
 
 @Component({
   selector: 'app-category',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categoryId:any;
+  category:any;
+  page=1;
+  constructor(private data:DataService, private activatedRoute:ActivatedRoute, private rest:RestApiService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(res=>{
+      this.categoryId=res['id'];
+      this.getProducts();
+    });
+  }
+  get lower(){
+    return 10*(this.page-1)+1;
   }
 
+  get upper(){
+    return Math.min(10*this.page, this.category.totalProducts)
+  }
+
+  async getProducts(event ?:any){
+    if(event){
+      this.category=null;
+    }
+    try {
+      const data=await this.rest.get(
+        `http://localhost:3030/api/categories/${this.categoryId}?page=${this.page-1}`
+      );
+      data['success']?(this.category=data):this.data.error(data["message"]);
+    } catch (error) {
+      this.data.error(error["message"]);
+    }
+  }
 }

9756d7291b9dc6e739edc0b2908b6e9e6e0bf7d6
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-19 16:00:12 +0530

    Added review component logic

--- Client/webapp/src/app/product/product.component.ts
+++ Client/webapp/src/app/product/product.component.ts
@@ -11,6 +11,14 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class ProductComponent implements OnInit {
   product:any;
 
+  myReview={
+    title:'',
+    description:'',
+    rating:0
+  };
+
+  btnDisabled=false;
+
   constructor(private activatedRoute:ActivatedRoute, private data:DataService, private rest:RestApiService, private router:Router) { }
 
   async ngOnInit(){
@@ -23,4 +31,22 @@ export class ProductComponent implements OnInit {
     })
   }
 
+  async postReview(){
+    this.btnDisabled=true;
+    try {
+      const data=await this.rest.post(
+        "http://localhost:3030/api/review",
+        {
+          productId:this.product._id,
+          title:this.myReview.title,
+          description:this.myReview.description,
+          rating:this.myReview.rating
+        }
+      );
+      data["success"]? this.data.success(data["message"]):this.data.error(data["message"]);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
+    this.btnDisabled=false;
+  }
 }

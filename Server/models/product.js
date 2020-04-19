00c236fe7ac1360a0a237a3a64c11fe33a32e1e3
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-04-19 15:40:34 +0530

    Added product avarage rate

--- Server/models/product.js
+++ Server/models/product.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
+const deepPopulate=require("mongoose-deep-populate")(mongoose);
+
 const ProductSchema=new Schema({
     category: {type:Schema.Types.ObjectId, ref:'Category'},
     owner: {type:Schema.Types.ObjectId, ref:'User'},
@@ -10,6 +12,26 @@ const ProductSchema=new Schema({
     description:String,
     price:Number,
     created:{type:Date, default:Date.now}
-})
+},{
+    toObject:{virtuals:true},
+    toJSON:{virtuals:true}
+});
+
+ProductSchema
+    .virtual('averageRating')
+    .get(function(){
+        var rating=0;
+        if(this.reviews.length==0){
+            rating=0;
+        }else{
+            this.reviews.map((review)=>{
+                rating+=review.rating;
+            });
+            rating=rating/this.reviews.length;
+        }
+        return rating;
+    });
+
+ProductSchema.plugin(deepPopulate);
 
 module.exports=mongoose.model("Product", ProductSchema);
